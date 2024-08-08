a0101bd49a5fc84a905f3285c15f7914de39b6d6
12nagrajhm <121280724+12nagrajhm@users.noreply.github.com>
2024-08-08 22:39:59 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+// src/App.jsx
+
+import React from "react";
+import KanbanBoard from "./components/KanbanBoard";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <KanbanBoard />
+    </div>
+  );
+}
+
+export default App;
